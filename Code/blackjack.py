610fdc70abd7f342a282a45a9cd2ef5824bf19d1
jacob <jacoblam87@gmail.com>
2024-02-01 22:12:39 +0800

    Adding in initial codes and file structure

---
+++ Code/blackjack.py
@@ -0,0 +1,21 @@
+BlackjackGame = input("Hello welcome to the game of Blackjack, would you like me to explain the rules? Please type either Yes or No: ")
+
+Cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K", "A"]
+Playerpoints: 
+Dealerpoints: 
+
+if BlackjackGame == "Yes":
+    print("The rules of Blackjack is simple in summary when all cards are drawn, whoever has a total closer to 21 than the dealer wins. If player's hand and dealer's hand have an equal value, it's a tie. ")
+    ans1 = input("Are you ready to begin? (Yes or No): ")
+    if ans1 == "Yes":
+        print("Okay lets start!")
+    carddeal1 = input("You have a", rand)
+    else:
+        print("Please let me know when your ready. For now the game will terminate")
+else:
+    input("Lets begin the games? (Yes or No): ")
+    if input == "Yes":
+        print("Okay")
+
+
+
